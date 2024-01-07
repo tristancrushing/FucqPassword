28079824ab6af0e2042691943c1610dc0bd98703
tristancrushing <36876912+tristancrushing@users.noreply.github.com>
2024-01-07 03:17:09 -0600

    Create IpspyProductionApiKeys.php
    Genesis of ipspyProductionApiKeys.php

---
+++ IpspyProductionApiKeys.php
@@ -0,0 +1,30 @@
+<?php
+class IpspyProductionApiKeys {
+    private $apiKeys = [];
+    private $filePath = 'ipspyProdApiKeys.json';
+
+    public function __construct() {
+        $this->loadApiKeys();
+    }
+
+    private function loadApiKeys() {
+        if (file_exists($this->filePath)) {
+            $this->apiKeys = json_decode(file_get_contents($this->filePath), true) ?: [];
+        }
+    }
+
+    public function getApiKeys() {
+        return $this->apiKeys;
+    }
+
+    public function addApiKey($apiKey) {
+        if (!in_array($apiKey, $this->apiKeys)) {
+            $this->apiKeys[] = $apiKey;
+            $this->saveApiKeys();
+        }
+    }
+
+    private function saveApiKeys() {
+        file_put_contents($this->filePath, json_encode($this->apiKeys));
+    }
+}

52fe1cdf2d2e3af714cc4a4d3a7bf8873a98d0ae
tristancrushing <36876912+tristancrushing@users.noreply.github.com>
2024-01-07 04:19:32 -0600

    Update IpspyProdApiKeyGen.php
    added return for $newKey

--- IpspyProdApiKeyGen.php
+++ IpspyProdApiKeyGen.php
@@ -23,5 +23,6 @@ class IpspyProdApiKeyGen {
         $newKey = self::generateApiKey();
         $keysManager = new IpspyProductionApiKeys();
         $keysManager->addApiKey($newKey);
+        return $newKey;
     }
 }
